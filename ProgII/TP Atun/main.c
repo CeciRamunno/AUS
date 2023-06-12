5c1ce2043f734fa7873d4b4eb92f982f0d423c75
CeciRamunno <59851663+CeciRamunno@users.noreply.github.com>
2023-06-12 07:30:24 -0300

    tp Atún

---
+++ ProgII/TP Atun/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+ // insertar(legislador, conj q corresp)
+    // suprimir(legislador, conj q corresp)
+    // bool miembro(legislador, conj)
+    // mostrar() lee arch y muestra pantalla
